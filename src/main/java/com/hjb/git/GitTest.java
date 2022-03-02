8261a90484e56513018a623cbd8d9b077ede8089
shine <huangjiabin2035@163.com>
2022-03-02 21:49:50 +0800

    third commit

--- src/main/java/com/hjb/git/GitTest.java
+++ src/main/java/com/hjb/git/GitTest.java
@@ -2,7 +2,8 @@ package com.hjb.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!!!!");
-        System.out.println("hello git!!!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
